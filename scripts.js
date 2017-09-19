8b9c94748e1bd9433e56965429d65966c1ccacad
katapo <kata.pochec@gmail.com>
2017-09-19 23:39:03 +0200

    first commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+var name = prompt('Enter your name');
+/*
+this is a sample multiple line comment
+*/
+alert('Hello, ' + name); // this is a sample single line comment
+/*
+console.log('Hello, ' + name);
+this is a sample multiple line comment to prevent execution
+/*
